b66ee2f74ca0faeeaf8e9fd345f85e287a24933e
Lukas Szimtenings <lszimtenings@ukaachen.de>
2020-06-25 13:38:00 +0200

    implemented basic infrastructure

---
+++ src/main/java/de/fhac/mazenet/client/logic/MessageHandler.java
@@ -0,0 +1,8 @@
+package de.fhac.mazenet.client.logic;
+
+/**
+ * Created by Lukas Szimtenings on 6/25/2020.
+ */
+public class MessageHandler
+{
+}
--- src/main/java/de/fhac/mazenet/client/logic/MessageRouter.java
+++ src/main/java/de/fhac/mazenet/client/logic/MessageRouter.java
@@ -1,8 +1,11 @@
 package de.fhac.mazenet.client.logic;
 
 import de.fhac.mazenet.server.generated.MazeCom;
+import de.fhac.mazenet.server.generated.MazeComMessagetype;
 
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Lukas Szimtenings on 6/25/2020.
@@ -10,13 +13,42 @@ import java.net.Socket;
 public class MessageRouter
 {
     private Socket socket;
-    
+    private Map<MazeComMessagetype, MessageHandler> handlerRegistry;
+
     public MessageRouter(Socket connectionToServer)
     {
         this.socket = connectionToServer;
+        handlerRegistry = new HashMap<MazeComMessagetype, MessageHandler>();
     }
     
-    public void route(MazeCom message){
     
+    public void register(MazeComMessagetype messagetype, MessageHandler handler){
+        this.handlerRegistry.put(messagetype, handler);
+    }
+    
+    
+    public void route(MazeCom message){
+        switch(message.getMessagetype()){
+            case LOGIN:
+                break;
+            case LOGINREPLY:
+                break;
+            case AWAITMOVE:
+                break;
+            case MOVE:
+                break;
+            case MOVEINFO:
+                break;
+            case GAMESTATUS:
+                break;
+            case CONTROLSERVER:
+                break;
+            case ACCEPT:
+                break;
+            case WIN:
+                break;
+            case DISCONNECT:
+                break;
+        }
     }
 }
