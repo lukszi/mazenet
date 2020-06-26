aa04914453fb90fc9f1e123d2c9bd6befd53d827
Marvin <Marvin@DESKTOP-57D6FDO>
2020-06-26 15:31:55 +0200

    lastposshift added
    Signed-off-by: Marvin

--- src/main/java/de/fhac/mazenet/client/ApplicationState.java
+++ src/main/java/de/fhac/mazenet/client/ApplicationState.java
@@ -1,13 +1,24 @@
 package de.fhac.mazenet.client;
 
+import de.fhac.mazenet.server.generated.PositionData;
+
 /**
  * Created by Lukas Szimtenings on 6/26/2020.
  */
 public class ApplicationState
 {
     private int id;
+    private PositionData lastshiftpos;
     
-    public int getId()
+    public PositionData getLastshiftpos() {
+		return lastshiftpos;
+	}
+
+	public void setLastshiftpos(PositionData lastshiftpos) {
+		this.lastshiftpos = lastshiftpos;
+	}
+
+	public int getId()
     {
         return id;
     }
