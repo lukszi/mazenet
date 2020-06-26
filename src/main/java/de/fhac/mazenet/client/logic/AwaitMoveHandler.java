cb22e97b55c7cfc1e12459bc27ab50491e2822c0
Lukas Szimtenings <lszimtenings@ukaachen.de>
2020-06-26 17:11:45 +0200

    basic algorithm implemented in half

--- src/main/java/de/fhac/mazenet/client/logic/AwaitMoveHandler.java
+++ src/main/java/de/fhac/mazenet/client/logic/AwaitMoveHandler.java
@@ -1,11 +1,15 @@
 package de.fhac.mazenet.client.logic;
 
+import de.fhac.mazenet.server.game.Board;
 import de.fhac.mazenet.server.game.Position;
 import de.fhac.mazenet.server.generated.*;
-import de.fhac.mazenet.server.networking.MazeComMessageFactory;
 import de.fhac.mazenet.server.networking.XmlOutputStream;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by Lukas Szimtenings on 6/25/2020.
@@ -28,7 +32,11 @@ public class AwaitMoveHandler extends MessageHandler
     	
         System.out.println("Awaiting movement");
         BoardData boardData = message.getAwaitMoveMessage().getBoard();
-        if(!(shiftpos.isLooseShiftPosition())&&getOppositeMove(boardData).equals(shiftpos)) {
+        Board board = new Board(boardData);
+        List<Board> boards = createFakeBoards(board);
+        
+        MoveMessageData mv = getMove(board);
+        if(!(shiftpos.isLooseShiftPosition())&& getInverseMove(boardData).equals(shiftpos)) {
         	//fehler
         }else {
         	
@@ -50,7 +58,44 @@ public class AwaitMoveHandler extends MessageHandler
         }  
     }
     
-    private Position getOppositeMove(BoardData boardData) {
+    private MoveMessageData getMove(Board originalBoard)
+    {
+        List<Board> boards = createFakeBoards(originalBoard);
+        boards.add((Board)originalBoard.clone());
+    
+        Map<Board, List<Position>> reachablePositions = boards.stream()
+                .collect(
+                        Collectors.toMap(
+                                board -> board,
+                                board -> board.getAllReachablePositions(board.findPlayer(STATE.getId()))));
+        
+        return null;
+    }
+    
+    private List<Board> createFakeBoards(Board board)
+    {
+        // Get all allowed shifts
+        ArrayList<MoveMessageData> allowedShifts = getAllAllowedShifts(board).stream()
+                .map(this::createMoveMessageData)
+                .collect(Collectors.toCollection(ArrayList::new));
+        
+        // Apply all allowed shifts
+        return allowedShifts.stream()
+                .map(board::fakeShift)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+    
+    private MoveMessageData createMoveMessageData(Position allowedShift)
+    {
+        return null;
+    }
+    
+    private List<Position> getAllAllowedShifts(Board board)
+    {
+        return null;
+    }
+    
+    private Position getInverseMove(BoardData boardData) {
     	Position pos=(Position)boardData.getForbidden();
     	return pos.getOpposite();
     }
