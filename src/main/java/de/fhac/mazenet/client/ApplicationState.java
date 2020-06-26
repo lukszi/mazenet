d8310457665cc4e0b311762ede44d235af680aa2
Lukas Szimtenings <lszimtenings@ukaachen.de>
2020-06-26 14:56:38 +0200

    Added application state

---
+++ src/main/java/de/fhac/mazenet/client/ApplicationState.java
@@ -0,0 +1,8 @@
+package de.fhac.mazenet.client;
+
+/**
+ * Created by Lukas Szimtenings on 6/26/2020.
+ */
+public class ApplicationState
+{
+}
--- src/main/java/de/fhac/mazenet/client/logic/MessageHandler.java
+++ src/main/java/de/fhac/mazenet/client/logic/MessageHandler.java
@@ -1,13 +1,15 @@
 package de.fhac.mazenet.client.logic;
 
+import de.fhac.mazenet.client.ApplicationState;
 import de.fhac.mazenet.server.generated.MazeCom;
 
-import java.net.Socket;
 
 /**
  * Created by Lukas Szimtenings on 6/25/2020.
  */
 public abstract class MessageHandler
 {
+    public static ApplicationState state;
+    
     public abstract void handle(MazeCom message);
 }
