fe39942a9ffed641bcd427ec1665e135566fa08c
Nicklas Felden-Hilfer <nicklas.felden-hilfer@reth-aachen.de>
2020-06-28 12:38:59 +0200

    "AwaitMoveHandler stuff"

--- src/main/java/de/fhac/mazenet/client/logic/AwaitMoveHandler.java
+++ src/main/java/de/fhac/mazenet/client/logic/AwaitMoveHandler.java
@@ -4,19 +4,16 @@ import de.fhac.mazenet.server.game.Board;
 import de.fhac.mazenet.server.game.Position;
 import de.fhac.mazenet.server.generated.*;
 import de.fhac.mazenet.server.networking.XmlOutputStream;
+import javafx.geometry.Pos;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Created by Lukas Szimtenings on 6/25/2020.
  */
-public class AwaitMoveHandler extends MessageHandler
-{
+public class AwaitMoveHandler extends MessageHandler {
     private XmlOutputStream out;
 
     public AwaitMoveHandler(XmlOutputStream out) {
@@ -24,88 +21,107 @@ public class AwaitMoveHandler extends MessageHandler
     }
 
     @Override
-    public void handle(MazeCom message)
-    {
-        //TODO: Implement
-    	Position shiftpos =new Position(0, 1);
-    	Position playerpos=new Position(5, 5);
-    	
-    	
+    public void handle(MazeCom message) {
         System.out.println("Awaiting movement");
+
         BoardData boardData = message.getAwaitMoveMessage().getBoard();
-        Treasure toFindTreasure = message.getAwaitMoveMessage().getTreasureToFindNext();
         Board board = new Board(boardData);
-        
-        MoveMessageData mv = getMove(board);
-        if(!(shiftpos.isLooseShiftPosition())&& getInverseMove(boardData).equals(shiftpos)) {
-        	//fehler
-        }else {
-        	
-            MoveMessageData move = new MoveMessageData();
-            move.setShiftCard(boardData.getShiftCard());
-            move.setNewPinPos(playerpos);
-            move.setShiftPosition(shiftpos);
-            
-            boardData.setForbidden(shiftpos);
-            STATE.setBoardData(boardData);
-            
-            MazeCom mazeCom = new MazeCom();
-            mazeCom.setMessagetype(MazeComMessagetype.MOVE);
-            mazeCom.setMoveMessage(move);
-            try {
-                out.write(mazeCom);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }  
+        Treasure treasureToFind = message.getAwaitMoveMessage().getTreasureToFindNext();
+
+        MoveMessageData move = getMove(board, treasureToFind);
+
+        MazeCom mazeCom = new MazeCom();
+        mazeCom.setMessagetype(MazeComMessagetype.MOVE);
+        mazeCom.setMoveMessage(move);
+        try {
+            out.write(mazeCom);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 
     private boolean treasureReachable(PositionData playerPos, PositionData treasurePos, Board board) {
         List<Position> reachable = board.getAllReachablePositions(playerPos);
         return reachable.contains(treasurePos);
     }
-    
-    private MoveMessageData getMove(Board originalBoard)
-    {
-        Map<Position, Board> boards = createFakeBoards(originalBoard);
-        
-        return null;
+
+    private MoveMessageData getMove(Board board, Treasure treasure) {
+        MoveMessageData move = null;
+        List<Board> fakeBoards = createFakeBoards(board);
+
+        for (Board fakeBoard : fakeBoards) {
+            Position playerPos = fakeBoard.findPlayer(STATE.getId());
+            PositionData treasurePosData = fakeBoard.findTreasure(treasure);
+            Position treasurePos = new Position(treasurePosData);
+
+            if (fakeBoard.getAllReachablePositions(playerPos).contains(treasurePos)) {
+                Position forbidden = new Position(fakeBoard.getForbidden());
+                Position shiftPos = forbidden.getOpposite();
+
+                move = new MoveMessageData();
+                move.setNewPinPos(treasurePos);
+                move.setShiftPosition(shiftPos);
+                move.setShiftCard(board.getShiftCard());
+            }
+        }
+        if (move == null) {
+            move = getRandomMove(board, treasure);
+        }
+        return move;
     }
-    
-    private Map<Position, Board> createFakeBoards(Board board)
-    {
-        // Get all allowed shifts
-        ArrayList<Position> allowedShifts = new ArrayList<>(getAllAllowedShifts(board));
-        
-        // Apply all allowed shifts
-        return allowedShifts.stream()
-                .collect(Collectors.toMap(shiftPos -> shiftPos, shiftPos -> createFakeBoard(board, shiftPos)));
+
+    private MoveMessageData getRandomMove(Board board, Treasure treasure) {
+        MoveMessageData move = new MoveMessageData();
+        Random rnd = new Random();
+        List<Board> fakeBoards = createFakeBoards(board);
+        Position playerPos = board.findPlayer(STATE.getId());
+        Board randomBoard = fakeBoards.get(rnd.nextInt(fakeBoards.size()));
+        List<Position> reachable = randomBoard.getAllReachablePositions(playerPos);
+        Position newPlayerPos = reachable.get(rnd.nextInt(reachable.size()));
+
+        move.setShiftPosition(randomBoard.getForbidden());
+        move.setShiftCard(board.getShiftCard());
+        move.setNewPinPos(newPlayerPos);
+
+        return move;
+
     }
-    
-    private Board createFakeBoard(Board board, Position shiftPos)
-    {
+
+
+    private List<Board> createFakeBoards(Board board){
+        ArrayList<Board> fakeBoards = new ArrayList<>();
+        List<Position> shifts = getAllShifts(board);
+        Position playerPos = board.findPlayer(STATE.getId());
+
+        for (Position shift : shifts){
+            MoveMessageData fakeMove = new MoveMessageData();
+            fakeMove.setShiftPosition(shift);
+            fakeMove.setShiftCard(board.getShiftCard());
+            fakeMove.setNewPinPos(playerPos);
+
+            Board fakeBoard = board.fakeShift(fakeMove);
+            fakeBoards.add(fakeBoard);
+        }
+
+        return fakeBoards;
+    }
+
+    private Board createFakeBoard(Board board, Position shiftPos) {
         MoveMessageData move = new MoveMessageData();
         move.setShiftPosition(shiftPos);
         board.fakeShift(move);
         return board;
     }
-    
-    private List<Position> getAllAllowedShifts(Board board)
-    {
-        List<Position> allShifts = getAllShifts(board);
-        return allShifts.stream()
-                .filter(position -> position.equals(board.getForbidden()))
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-    
-    private List<Position> getAllShifts(Board board){
+
+    private List<Position> getAllShifts(Board board) {
         ArrayList<Position> shiftPositions = new ArrayList<>();
-        for(int col = 0; col < 6; col++){
-            for(int row = 0; row < 6; row++){
+        for (int col = 0; col < 6; col++) {
+            for (int row = 0; row < 6; row++) {
                 Position position = new Position(row, col);
-                if (!(position == board.getForbidden())){
+                if (!(position == board.getForbidden())) {
                     if (position.getCol() % 6 == 0) {
-                        if (position.getRow() % 2 == 0){
+                        if (position.getRow() % 2 == 0) {
                             shiftPositions.add(position);
                         }
                     } else if (position.getRow() % 6 == 0) {
@@ -118,10 +134,10 @@ public class AwaitMoveHandler extends MessageHandler
         }
         return shiftPositions;
     }
-    
+
     private Position getInverseMove(BoardData boardData) {
-    	Position pos=(Position)boardData.getForbidden();
-    	return pos.getOpposite();
+        Position pos = (Position) boardData.getForbidden();
+        return pos.getOpposite();
     }
 
 }
